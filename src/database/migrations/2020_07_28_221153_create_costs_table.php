56dfae7f0b7a72fae7917bf8d58c5214144cb84c
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-07-29 07:30:59 +0900

    costsテーブルのマイグレーション作成

---
+++ src/database/migrations/2020_07_28_221153_create_costs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('costs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('accountName', 45);
+            $table->integer('price');
+            $table->string('journal', 45);
+            $table->int('year');
+            $table->int('month');
+            $table->int('day');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('costs');
+    }
+}
