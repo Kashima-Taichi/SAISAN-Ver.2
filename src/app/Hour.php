66d6b097a229d1311ee7169dd7a09605e22f1488
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-08-03 08:42:42 +0900

    稼働時間テーブルのモデルファイルの作成・編集

---
+++ src/app/Hour.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hour extends Model
+{
+    //テーブル設定
+    protected $table = 'hours';
+
+    // ホワイトリストの設定
+    protected $fillable = [
+        'year',
+        'month',
+        'yearMonth',
+        'fixedTime',
+        'overTime',
+    ];
+}
