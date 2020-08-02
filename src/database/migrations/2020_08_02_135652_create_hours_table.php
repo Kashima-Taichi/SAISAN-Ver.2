2a0a55114da8cd648cf264cf0f507c282f4cfd17
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-08-03 08:38:10 +0900

    稼働時間計上テーブルのマイグレーションファイルの作成・編集

---
+++ src/database/migrations/2020_08_02_135652_create_hours_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hours', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('year');
+            $table->integer('month');
+            $table->integer('yearMonth');
+            $table->double('fixedTime', 8, 2);
+            $table->double('overTime', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hours');
+    }
+}
