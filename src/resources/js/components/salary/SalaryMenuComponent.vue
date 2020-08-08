839d5d904de786f1f7e5374ce038d2482134d39a
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-08-08 23:17:23 +0900

    収入メニューのコンポーネントの実装

---
+++ src/resources/js/components/salary/SalaryMenuComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <table class="table table-hover">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">コンテンツ</th>
+          <th scope="col">アクション</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">収入計上</th>
+          <td>
+            <router-link v-bind:to="{name: 'salary.rec'}">
+              <button class="btn btn-primary">GO!</button>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
