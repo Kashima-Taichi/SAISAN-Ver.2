48bb35bf2334daa1c2148c1fe7d5b156dd9e7a7a
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-08-08 20:51:13 +0900

    その他の部のメニューを追加する

---
+++ src/resources/js/components/other/OtherMenuComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <table class="table table-hover">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">コンテンツ</th>
+          <th scope="col">アクション</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">データベースのダンプ</th>
+          <td>
+            <router-link v-bind:to="{name: 'other.dump'}">
+              <button class="btn btn-primary">GO!</button>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
