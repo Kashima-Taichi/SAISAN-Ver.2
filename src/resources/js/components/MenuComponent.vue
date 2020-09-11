6364169bdb0ebec819a940a0c444d8bdf79ab99b
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-09-11 23:10:20 +0900

    メニューページの文言の修正

--- src/resources/js/components/MenuComponent.vue
+++ src/resources/js/components/MenuComponent.vue
@@ -9,7 +9,7 @@
       </thead>
       <tbody>
         <tr>
-          <th scope="row">支出ノ部</th>
+          <th scope="row">経費関係</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.cost'}">
               <button class="btn btn-primary">GO!</button>
@@ -17,7 +17,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">収入ノ部</th>
+          <th scope="row">所得関係</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.salary'}">
               <button class="btn btn-primary">GO!</button>
@@ -25,7 +25,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">賞与ノ部</th>
+          <th scope="row">賞与関係</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.bonus'}">
               <button class="btn btn-primary">GO!</button>
@@ -33,7 +33,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">時間ノ部</th>
+          <th scope="row">稼働時間関係</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.hour'}">
               <button class="btn btn-primary">GO!</button>
@@ -41,7 +41,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">勘定科目ノ部</th>
+          <th scope="row">勘定科目関係</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.account'}">
               <button class="btn btn-primary">GO!</button>
@@ -49,7 +49,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">PLノ部</th>
+          <th scope="row">PL参照</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.pl'}">
               <button class="btn btn-primary">GO!</button>
@@ -57,7 +57,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">グラフノ部</th>
+          <th scope="row">グラフ参照</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.graph'}">
               <button class="btn btn-primary">GO!</button>
@@ -65,15 +65,7 @@
           </td>
         </tr>
         <tr>
-          <th scope="row">勘定科目ノ部</th>
-          <td>
-            <router-link v-bind:to="{name: 'saisan.account'}">
-              <button class="btn btn-primary">GO!</button>
-            </router-link>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">その他ノ部</th>
+          <th scope="row">その他</th>
           <td>
             <router-link v-bind:to="{name: 'saisan.other'}">
               <button class="btn btn-primary">GO!</button>
